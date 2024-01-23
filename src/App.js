87c91e2dca85c7543dc8e336a8625de2c34b8523
Your Name <you@example.com>
2024-01-23 19:07:05 +0100

    made populareRecipies.js responsive

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
-import Nav from './nav.js'
+import Nav from './nav.js';
 import PopularRecipies from './popularRecipies.js';
-
+import menuVisible from './nav.js';
+console.log(menuVisible)
 function App() {
   return (
-    <div className="App">
-      <Nav />
+    
+    <div className="App md:overflow-visible overflow-hidden">
+      <Nav style={{ zIndex: '999' }} />
       <PopularRecipies />
     </div>
+
   );
 }
 
--- src/popularRecipies.js
+++ src/popularRecipies.js
@@ -8,8 +8,8 @@ import food2 from './imgs/food2.avif';
 
 const popularRecipies=() =>{
     return (
-        <div class="flex flex-col-2 h-[80vh] mt-12 md:my-12 bg-opacity-20   ">
-            <div class="w-[60%] pl-8">
+        <div class="flex flex-col-2 md:h-[80vh] h-[35%] md:mt-12 mt-8 md:my-12 bg-opacity-20   ">
+            <div class="w-[60%] md:pl-8 pl-4">
                 <p class="text-black text-lg md:text-4xl w-full h-fit md:my-5 my-2 text-left ">let's start cooking with </p>
                 <p class="uppercase text-2xl md:text-7xl w-full text-left text-[#E1611F] md:my-5 my-2 ">Popular Recipies</p>
                 <p class="text-black-500 text-sm w-full h-fit  text-left  md:my-5 my-2 md:max-w-[50%]">Welcome to FlavZ, your ultimate destination for culinary inspiration and mouthwatering recipes! Whether you're a seasoned chef or a kitchen novice, our food haven is designed to spark creativity.</p>
@@ -36,6 +36,7 @@ const popularRecipies=() =>{
                                 md:max-w-[70%] 
                                 justify-center 
                                 flex
+                                text-center
                                 ">
                                     Discover Recipies   
                     </button>
@@ -44,8 +45,8 @@ const popularRecipies=() =>{
                 <img src={food2}  class=" invisible md:visible w-48 h-48 rounded-full ml-52 " />
                 <div class= " invisible md:visible absolute w-52 h-52 rounded-full left-52 top-0 bg-transparent ring-2 ring-orange-600"></div>
                 
-                <img src={food1}  class=" md:w-72 md:h-72 w-32 h-32 rounded-full  ml-4 absolute md:reltative bottom-14 md:top-44 md:bottom-5 " />
-                <div class="absolute md:w-80 md:h-80 w-36 h-36 rounded-full  md:top-40 top-24  bg-transparent ring-2 ring-orange-600"></div>
+                <img src={food1} class=" border-4 md:w-72 md:h-72 w-32 h-32 rounded-full  ml-4 absolute md:reltative top-24 md:top-44 md:bottom-5 " />
+                <div class="absolute md:w-80 md:h-80 w-36 h-36 rounded-full  md:top-40 top-24  bg-transparent ring-2 bottom-14 ring-orange-600"></div>
             </div>
 
 
